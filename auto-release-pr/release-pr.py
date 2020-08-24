979c3b093319e05986553486c2641f202b24889b
andooown <andooown.dev@gmail.com>
2020-08-24 17:02:31 +0900

    Pass args for the number of PR

--- auto-release-pr/release-pr.py
+++ auto-release-pr/release-pr.py
@@ -7,8 +7,12 @@ import re
 import sys
 
 
-GITHUB_TOKEN = os.environ['INPUT_GITHUBTOKEN']
-REPO_NAME = os.environ['GITHUB_REPOSITORY']
+GITHUB_TOKEN: str = os.environ['INPUT_GITHUBTOKEN']
+REPO_NAME: str = os.environ['GITHUB_REPOSITORY']
+BASE_BRANCH: str = os.environ['INPUT_BASEBRANCH']
+HEAD_BRANCH: str = os.environ['INPUT_HEADBRANCH']
+RELEASE_PR_NUMBER: Optional[str] = os.environ.get('INPUT_RELEASEPRNUMBER')
+
 
 COMMENT_TEMPLATE = '''PR body is updated!
 <details><summary>diff</summary>
@@ -24,8 +28,8 @@ COMMENT_TEMPLATE = '''PR body is updated!
 
 
 # release 向きの最新 PR を取得
-def find_latest_release_pr(repo: github.Repository.Repository) -> Optional[github.PullRequest.PullRequest]:	
-    prs = repo.get_pulls(state='open', base='release', head='master', sort='created', direction='desc')	
+def find_latest_release_pr(repo: github.Repository.Repository, base: str, head: str) -> Optional[github.PullRequest.PullRequest]:	
+    prs = repo.get_pulls(state='open', base=base, head=head, sort='created', direction='desc')	
 
     if prs.totalCount > 0:	
         return prs[0]	
@@ -33,7 +37,10 @@ def find_latest_release_pr(repo: github.Repository.Repository) -> Optional[githu
         return None
 
 # release 向きの最新 PR を取得を探して、なかったら作成する
-def find_or_create_release_pr(repo: github.Repository.Repository) -> github.PullRequest.PullRequest:	
+def find_or_create_release_pr(repo: github.Repository.Repository, base: str, head: str, number: Optional[str]) -> github.PullRequest.PullRequest:	
+    if number:
+        return repo.get_pull(int(number))
+
     latest = find_latest_release_pr(repo)
     if latest:
         return latest
@@ -67,7 +74,7 @@ def make_new_body(pr: github.PullRequest.PullRequest) -> Optional[str]:
 def main():
     g = github.Github(GITHUB_TOKEN)
     repo = g.get_repo(REPO_NAME)
-    release_pr = find_or_create_release_pr(repo)
+    release_pr = find_or_create_release_pr(repo, base=BASE_BRANCH, head=HEAD_BRANCH, number=RELEASE_PR_NUMBER)
 
     # body を生成
     new_body = make_new_body(release_pr)
