510c0f53c2287083e15288ae0a1e35f22b44acef
Tashiro <g4160hc@gmail.com>
2022-03-04 09:18:07 +0900

    コメント時のテンプレートに新規PRの情報を渡せるように

--- auto-release-pr/release-pr.py
+++ auto-release-pr/release-pr.py
@@ -88,12 +88,13 @@ def main():
     old_body_lines = (release_pr.body if release_pr.body else '').splitlines()
     new_body_lines = new_body.splitlines() 
     diff = '\n'.join(difflib.unified_diff(old_body_lines, new_body_lines))
-
+    new_line = [line for line in new_body_lines if line not in old_body_lines][0]
+   
     # PR 本文を更新
     release_pr.edit(body=new_body)
 
     # comment に diff を残す
-    comment_body = COMMENT_TEMPLATE.format(diff=diff)
+    comment_body = COMMENT_TEMPLATE.format(diff=diff, new_line=new_line)
     release_pr.as_issue().create_comment(comment_body)
 
 
